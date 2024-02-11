276bfa055ce09f96ad00f0cbf2e35bf05bdbdf4f
Alex Venutelli <alex.venutelli@outlook.com>
2024-02-11 18:39:08 +0100

    modifiche

---
+++ pokedex-progetto-react/src/component/pokemonList/PokemonList.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { URL_POKEMON_151 } from "@/api/apiPokemon";
+import Pokemon from "../pokemon/Pokemon";
+import styles from "./index.module.scss";
+
+const PokemonList = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch(URL_POKEMON_151)
+      .then((res) => res.json())
+      .then((data) => setPokemonList(data.results));
+  }, []);
+
+  return (
+    <div className={styles.PokemonList}>
+      {pokemonList.map((pokemon) => (
+        <Pokemon pokemonData={pokemon} key={pokemon.name} />
+      ))}
+    </div>
+  );
+};
+
+export default PokemonList;
+// import { useState, useEffect } from "react";
+// import { URL_POKEMON } from "@/api/apiPokemon";
+// import Pokemon from "../pokemon/Pokemon";
+// import styles from "./index.module.scss";
+
+// const PokemonList = () => {
+//   const [pokemonList, setPokemonList] = useState([]);
+
+//   useEffect(() => {
+//     fetch(URL_POKEMON)
+//       .then((res) => res.json())
+//       .then((data) => setPokemonList(data.results));
+//   }, []);
+
+//   return (
+//     <div className={styles.PokemonList}>
+//       {pokemonList.map((pokemon) => (
+//         <Pokemon pokemonData={pokemon} key={pokemon.name} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default PokemonList;
